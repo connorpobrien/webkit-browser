b7a258c561d811c9ec51b23ac9cc852a8493801a
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2023-12-22 00:37:57 -0800

    first commit

---
+++ webkit-browser/webkit_browserApp.swift
@@ -0,0 +1,32 @@
+//
+//  webkit_browserApp.swift
+//  webkit-browser
+//
+//  Created by Connor O'Brien on 12/22/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct webkit_browserApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
